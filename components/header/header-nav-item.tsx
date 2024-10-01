f5471daa0f4daa34d0323e6bcc63dd83ee59f9ee
leduardovb <luizbarreto@viptech.com.br>
2024-09-30 22:20:51 -0300

    feat(components): add tooltip component and add tooltip action in nav item

--- components/header/header-nav-item.tsx
+++ components/header/header-nav-item.tsx
@@ -4,31 +4,47 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { Button } from "../ui/button";
 import { cn } from "@/lib/utils";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "../ui/tooltip";
 
 interface Props {
+  title: string;
   href: string;
 }
 
 export function HeaderNavItem({
   href,
+  title,
   children,
 }: React.PropsWithChildren<Props>) {
   const pathname = usePathname();
   const isActive = pathname === href;
 
   return (
-    <Link href={href} className="flex items-center justify-center">
-      <Button
-        size={"icon"}
-        variant={"ghost"}
-        className={cn(
-          isActive
-            ? "text-primary hover:text-primary/90"
-            : "text-primary-foreground",
-        )}
-      >
-        {children}
-      </Button>
-    </Link>
+    <TooltipProvider>
+      <Tooltip>
+        <TooltipTrigger asChild>
+          <Link href={href} className="flex items-center justify-center">
+            <Button
+              size={"icon"}
+              variant={"ghost"}
+              className={cn(
+                "rounded-none transition-shadow hover:bg-transparent hover:shadow-[inset_0_-2px_0_#015F43]",
+                isActive
+                  ? "text-primary shadow-[inset_0_-2px_0_#015F43] hover:text-primary/90"
+                  : "text-primary-foreground",
+              )}
+            >
+              {children}
+            </Button>
+          </Link>
+        </TooltipTrigger>
+        <TooltipContent align="center">{title}</TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
   );
 }
